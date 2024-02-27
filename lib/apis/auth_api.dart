65b943d84a58781a6e5e23a89aec2a2faf9477be
Shonu72 <shourya7250@gmail.com>
2024-02-27 21:37:45 +0530

    android localhost fixed

--- lib/apis/auth_api.dart
+++ lib/apis/auth_api.dart
@@ -37,7 +37,7 @@ class AuthAPI implements IAuthAPI {
   Future<User?> currentUser() async {
     try {
       return await _account.get();
-    } on AppwriteException catch (e) {
+    } on AppwriteException {
       return null;
     } catch (e) {
       return null;
