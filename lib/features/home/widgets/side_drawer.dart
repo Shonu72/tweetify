265572b25e7121987b3d51fe6b7efadc45912acf
Shonu72 <shourya7250@gmail.com>
2024-03-24 01:17:11 +0530

    minor ui changes

--- lib/features/home/widgets/side_drawer.dart
+++ lib/features/home/widgets/side_drawer.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tweetify/features/auth/controller/auth_controller.dart';
 import 'package:tweetify/features/profile/controller/user_profile_controller.dart';
 import 'package:tweetify/features/profile/views/user_profile_view.dart';
+import 'package:tweetify/theme/pallete.dart';
 
 class SideDrawer extends ConsumerWidget {
   const SideDrawer({super.key});
@@ -11,70 +12,134 @@ class SideDrawer extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final currentUser = ref.watch(currentUserDeailsProvider).value;
     return SafeArea(
-      child: Column(
-        children: [
-          // CircleAvatar(
-          //   radius: 50,
-          //   backgroundImage: ,
-
-          // ),
-          const SizedBox(
-            height: 50,
-          ),
-          ListTile(
-            leading: const Icon(
-              Icons.person,
-              size: 30,
-            ),
-            title: const Text(
-              'My Profile',
-              style: TextStyle(
-                fontSize: 22,
+      child: Drawer(
+        backgroundColor: Pallete.backgroundColor,
+        child: Padding(
+          padding: const EdgeInsets.only(left: 20, top: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CircleAvatar(
+                // radius: 50,
+                backgroundImage: NetworkImage(
+                  currentUser!.profilePic,
+                ),
               ),
-            ),
-            onTap: () {
-              Navigator.push(context, UserProfileView.route(currentUser!));
-            },
-          ),
-          ListTile(
-            leading: const Icon(
-              Icons.payments,
-              size: 30,
-            ),
-            title: const Text(
-              'Twitter Blue',
-              style: TextStyle(
-                fontSize: 22,
+              const SizedBox(
+                height: 5,
               ),
-            ),
-            onTap: () {
-              ref
-                  .read(userProfileControllerProvider.notifier)
-                  .updateUserProfile(
-                      userModel: currentUser!.copyWith(
-                        isTwitterBlue: true,
-                      ),
-                      context: context,
-                      bannerImage: null,
-                      profileImage: null);
-            },
-          ),
-          ListTile(
-            leading: const Icon(
-              Icons.logout,
-              size: 30,
-            ),
-            title: const Text(
-              'Logout',
-              style: TextStyle(
-                fontSize: 22,
+              Text(
+                currentUser.name,
+                style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            onTap: () {
-              ref.read(authControllerProvider.notifier).logout(context);
-            },
+              Text(
+                '@${currentUser.name}',
+                style: const TextStyle(fontSize: 16, color: Pallete.greyColor),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Row(
+                children: [
+                  Text(
+                    currentUser.following.length.toString(),
+                    style: const TextStyle(
+                      fontSize: 16,
+                      color: Pallete.whiteColor,
+                    ),
+                  ),
+                  const Text(
+                    ' Following',
+                    style: TextStyle(fontSize: 16, color: Pallete.greyColor),
+                  ),
+                  const SizedBox(
+                    width: 5,
+                  ),
+                  const SizedBox(
+                    width: 5,
+                  ),
+                  Text(
+                    currentUser.followers.length.toString(),
+                    style: const TextStyle(
+                      fontSize: 16,
+                      color: Pallete.whiteColor,
+                    ),
+                  ),
+                  const Text(
+                    ' Followers',
+                    style: TextStyle(fontSize: 16, color: Pallete.greyColor),
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                ],
+              ),
+              const Divider(
+                color: Pallete.greyColor,
+                thickness: 0.5,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              ListTile(
+                leading: const Icon(
+                  Icons.person,
+                  size: 30,
+                ),
+                title: const Text(
+                  'My Profile',
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+                onTap: () {
+                  Navigator.push(context, UserProfileView.route(currentUser!));
+                },
+              ),
+              ListTile(
+                leading: const Icon(
+                  Icons.payments,
+                  size: 30,
+                ),
+                title: const Text(
+                  'Twitter Blue',
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+                onTap: () {
+                  ref
+                      .read(userProfileControllerProvider.notifier)
+                      .updateUserProfile(
+                          userModel: currentUser.copyWith(
+                            isTwitterBlue: true,
+                          ),
+                          context: context,
+                          bannerImage: null,
+                          profileImage: null);
+                },
+              ),
+              ListTile(
+                leading: const Icon(
+                  Icons.logout,
+                  size: 30,
+                ),
+                title: const Text(
+                  'Logout',
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+                onTap: () {
+                  ref.read(authControllerProvider.notifier).logout(context);
+                },
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
